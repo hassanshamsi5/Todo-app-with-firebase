869112e58c2582228398b9c067c85b402ccee348
hassanshamsi5 <shamsihassan5@gmail.com>
2023-08-05 00:02:42 +0500

    changes

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { collection, addDoc, getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
-
-// import { async } from "q";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-// import {getFirestore} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js"
-
-
+import { collection, addDoc, getFirestore, updateDoc, getDocs, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
@@ -19,18 +13,14 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
 const db = getFirestore(app);
-
-
-
-
-
 
 async function getAndUpdate() {
   console.log("Updating List...");
   let tit = document.getElementById('title').value;
   let desc = document.getElementById('description').value;
+
+
   let itemJsonArray;
   try {
     const docRef = await addDoc(collection(db, "Todos"), {
@@ -48,6 +38,7 @@ async function getAndUpdate() {
     itemJsonArray = [];
     itemJsonArray.push([tit, desc]);
     localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+
   } else {
     let itemJsonArrayStr = localStorage.getItem('itemsJson');
     itemJsonArray = JSON.parse(itemJsonArrayStr);
@@ -58,6 +49,7 @@ async function getAndUpdate() {
 }
 
 function update() {
+
   let itemJsonArray;
   if (localStorage.getItem('itemsJson') == null) {
     itemJsonArray = [];
@@ -68,30 +60,59 @@ function update() {
   let tableBody = document.getElementById("tableBody");
   let str = "";
   itemJsonArray.forEach((element, index) => {
-    str += `
-        <tr>
-          <th scope="row">${index + 1}</th>
+    str +=
+      `<tr>
+         <th scope="row">${index + 1}</th>
           <td>${element[0]}</td>
           <td>${element[1]}</td>
           <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td>
-         
-        </tr>
-      `;
+          <td><button class="btn btn-sm btn-primary" onclick="editTask(${index})">Update Task</button></td>
+        </tr>`
+      ;
   });
   tableBody.innerHTML = str;
 }
+
+function editTask(index) {
+  var tableBodyRows = document.querySelectorAll("#tableBody tr");
+  if (index < 0 || index >= tableBodyRows.length) {
+    console.error("Invalid index:", index);
+    return;
+  }
+
+  var listItem = tableBodyRows[index];
+  var taskTitle = listItem.children[1];
+  var taskDescription = listItem.children[2];
+  var newText = prompt("Edit title:", taskTitle.textContent);
+
+  if (newText !== null && newText.trim() !== "") {
+    taskTitle.textContent = newText;
+    taskDescription.textContent = prompt("Edit description:", taskDescription.textContent);
+    updateTaskInLocalStorage(index, newText, taskDescription.textContent); // Update in local storage
+    updateTaskInFirestore(index, newText, taskDescription.textContent); // Update in Firebase Firestore
+
+  }
+}
+window.editTask = editTask;
+
 let add = document.getElementById("add");
 add.addEventListener("click", getAndUpdate);
 
 update();
 
+
 function deleted(itemIndex) {
-  console.log("Delete", itemIndex);
-  let itemJsonArrayStr = localStorage.getItem('itemsJson');
-  let itemJsonArray = JSON.parse(itemJsonArrayStr);
-  itemJsonArray.splice(itemIndex, 1);
-  localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
-  update();
+  // Ask for confirmation before deleting the item
+  if (confirm("Are you sure you want to delete this item?")) {
+    console.log("Delete", itemIndex);
+
+    let itemJsonArrayStr = localStorage.getItem('itemsJson');
+    let itemJsonArray = JSON.parse(itemJsonArrayStr);
+    itemJsonArray.splice(itemIndex, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+
+    update();
+  }
 }
 
 function clearStorage() {
@@ -107,3 +128,48 @@ window.deleted = deleted
 
 
 
+
+function updateTaskInLocalStorage(index, newTitle, newDescription) {
+  let itemJsonArray;
+  if (localStorage.getItem('itemsJson') == null) {
+    itemJsonArray = [];
+  } else {
+    let itemJsonArrayStr = localStorage.getItem('itemsJson');
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+  }
+
+  // Update the item in the local storage array
+  itemJsonArray[index] = [newTitle, newDescription];
+  localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+
+  // Update the table on the webpage
+  update();
+}
+
+async function updateTaskInFirestore(index, newTitle, newDescription) {
+  try {
+    const querySnapshot = await getDocs(collection(db, "Todos"));
+    const docId = querySnapshot.docs[index].id;
+    const docRef = doc(db, "Todos", docId);
+    await updateDoc(docRef, {
+      value: newTitle,
+      description: newDescription,
+    });
+    console.log("Document updated with ID: ", docRef.id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+async function deleteTaskFromFirestore(index) {
+  try {
+    const querySnapshot = await getDocs(collection(db, "Todos"));
+    const docId = querySnapshot.docs[index].id;
+    const docRef = doc(db, "Todos", docId);
+    await deleteDoc(docRef);
+    console.log("Document deleted with ID: ", docRef.id);
+  } catch (err) {
+    console.log(err);
+  }
+}
